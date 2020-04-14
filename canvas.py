9674b5f17024d8f2f8b1c840ce75a2434b6704b5
Zein-Haddad <zeinhaddad02@gmail.com>
2020-04-14 08:05:30 +0700

    Fully added line tool

--- canvas.py
+++ canvas.py
@@ -1,12 +1,16 @@
 import kivy
 
 from kivy.uix.screenmanager import Screen
-from kivy.graphics import Color, Ellipse, Rectangle, Line
+from kivy.graphics import Color, Ellipse, Rectangle, Line, InstructionGroup
 
 class Canvas(Screen):
     tool_color = [1, 1, 1, 1]
     tool_size = 5
     current_tool = 'Pen tool'
+
+    line_start = []
+    lines = []
+
     def on_touch_down(self, touch):
         if self.current_tool == 'Pen tool':
             with self.canvas:
@@ -24,6 +28,7 @@ class Canvas(Screen):
             with self.canvas:
                 Color(self.tool_color[0], self.tool_color[1], self.tool_color[2], self.tool_color[3])
                 touch.ud['line'] = Line(points=(touch.x, touch.y), width=(self.tool_size / 2))
+                self.line_start = [touch.x, touch.y]
 
     def on_touch_move(self, touch):
         if self.current_tool == 'Pen tool':
@@ -38,10 +43,29 @@ class Canvas(Screen):
             except:
                 pass
 
+        if self.current_tool == 'Line tool':
+            try:
+                if len(self.lines) > 1:
+                    item = self.lines.pop(0)
+                    self.canvas.remove(item)
+
+                line = InstructionGroup()
+                line.add(Color(self.tool_color[0], self.tool_color[1], self.tool_color[2], self.tool_color[3]))
+                line.add(Line(points=(self.line_start[0], self.line_start[1], touch.x, touch.y), width=(self.tool_size / 2)))
+                self.lines.append(line)
+                self.canvas.add(line)
+            except:
+                pass
+
     def on_touch_up(self, touch):
         if self.current_tool == 'Line tool':
             try:
                 touch.ud['line'].points += [touch.x, touch.y]
+
+                if len(self.lines) > 0:
+                    for line in self.lines:
+                        self.canvas.remove(line)
+                        self.lines.remove(line)
             except:
                 pass
 
